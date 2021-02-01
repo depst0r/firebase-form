abfeb0b9950b71e9efb144f64b83ddce2ce2397f
depst0r <depstor90@gmail.com>
2021-02-01 16:11:11 +0300

    switch

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-'use strict'
-
 window.addEventListener('DOMContentLoaded', () => {
+    'use strict'
+
     const form = document.querySelector('form'),
         firstName = document.getElementById('exampleInputFirstName'),
         lastName = document.getElementById('exampleInputLastName'),
         btnSubmit = document.querySelector('.btn'),
         email = document.getElementById('exampleInputEmail1'),
         pass = document.getElementById('exampleInputPassword1');
-
 
 // function postDB(formData) {
 //     fetch('https://js-form-7807a-default-rtdb.firebaseio.com/form.json', {
@@ -21,39 +20,62 @@ window.addEventListener('DOMContentLoaded', () => {
 //     .then(res => console.log(res))
 // }
 
+
+    function isValid(elem) {
+        switch (elem.id) {
+            case 'exampleInputFirstName':
+                elem.value.length >= 3 ? elem.nextElementSibling.textContent = ''
+                : ( elem.nextElementSibling.textContent = 'Min length 3 simbol')
+            case 'exampleInputLastName':
+                elem.value.length >= 3 ? elem.nextElementSibling.textContent = ''
+                : elem.nextElementSibling.textContent = 'Min length 3 simbol'
+            default:
+                break;
+        }
+        
+    }
+
+        for (let elements of form.elements) {
+            if (!elements.classList.contains('btn')) {
+                elements.addEventListener('input', () => {
+                    isValid(elements)
+                })
+        }
+        
+    };
+
 form.addEventListener('submit', submitHandler);
 
-function valid() {
-    // const objInputForm = {
-    //     firstName: firstName.value,
-    //     lastName: lastName.value,
-    //     email: email.value,
-    //     password: pass.value 
-    // }
-
-    if (firstName.value.length >= 5 && lastName.value.length >= 5) {
-        console.log('true')
-    } else {
-        console.log('false')
-    }
-}
-
 function submitHandler(event) {
+
     event.preventDefault()
 
-    valid()
-    // console.log(firstName.value)
-    // console.log(lastName.value)
-    // console.log(email.value)
-    // console.log(pass.value)
+    //    const objValueForm = {
+    //     firstName: firstName.value.trim(),
+    //     lastName: lastName.value.trim(),
+    //     email: email.value.trim(),
+    //     password: pass.value.trim() 
+    // }
 
-    // btnSubmit.addEventListener('click', () => {
-    //     postDB(email.value)
-    //     email.value = '',
-    //     pass.value = '',
-    //     btnSubmit.disabled = true
+    // postDB(objValueForm)
 
-    // })
- };
+        // lastName.value = '',
+        // firstName.value = '',
+        // email.value = '',
+        // pass.value = ''
+        // btnSubmit.disabled = true
+
+        for (let elements of form.elements) {
+            if (!elements.classList.contains('btn')) {
+                if (elements.value === '') {
+                    elements.nextElementSibling.textContent = 'This field is empty'
+            } else {
+                elements.nextElementSibling.textContent = ''
+            }
+        }
+        
+    };
+
+  };
 
 });
