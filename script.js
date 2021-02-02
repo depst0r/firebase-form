8666395936224c5dbaf67389880ad53a3bc530c1
depst0r <depstor90@gmail.com>
2021-02-02 17:14:48 +0300

    name

--- script.js
+++ script.js
@@ -2,42 +2,39 @@ window.addEventListener('DOMContentLoaded', () => {
     'use strict'
 
     const form = document.querySelector('form'),
-        firstName = document.getElementById('exampleInputFirstName'),
-        lastName = document.getElementById('exampleInputLastName'),
-        btnSubmit = document.querySelector('.btn'),
-        email = document.getElementById('exampleInputEmail1'),
-        pass = document.getElementById('exampleInputPassword1');
+        btnSubmit = document.querySelector('.btn');
+        // firstName = document.getElementById('exampleInputFirstName'),
+        // lastName = document.getElementById('exampleInputLastName'),
+      
+        // email = document.getElementById('exampleInputEmail1'),
+        // pass = document.getElementById('exampleInputPassword1');
 
-// function postDB(formData) {
-//     fetch('https://js-form-7807a-default-rtdb.firebaseio.com/form.json', {
-//         method: 'POST',
-//         body: JSON.stringify(formData),
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     })
-//     .then(res => res.json())
-//     .then(res => console.log(res))
-// }
+    function submitForm(formData) {
+        fetch('https://js-form-7807a-default-rtdb.firebaseio.com/form.json', {
+            method: 'POST',
+            body: JSON.stringify(formData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(res => console.log(res))
+    }
+
+    const objValueForm = {}
 
     function isValid(elem) {
         const _regExpEmail = /[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/igm;
-
-        const objValueForm = {
-            firstName: firstName.value.trim(),
-            lastName: lastName.value.trim(),
-            email: email.value.trim(),
-            password: pass.value.trim() 
-        }
+        const _regExpName = /^[A-Za-z0-9]{3,3}$/
 
         switch (elem.id) {
             case 'exampleInputFirstName':
-                elem.value.length >= 3 ? (elem.nextElementSibling.textContent = '', objValueForm.firstName = elem.value)
+                _regExpName.test(elem.value) ? (elem.nextElementSibling.textContent = '', objValueForm.firstName = elem.value)
                 : ( elem.nextElementSibling.textContent = 'Min length 3 simbol')
                 break;
             case 'exampleInputLastName':
-                elem.value.length >= 3 ? (elem.nextElementSibling.textContent = '', objValueForm.lastName = elem.value)
-                : elem.nextElementSibling.textContent = 'Min length 3 simbol'
+                _regExpName.test(elem.value) ? (elem.nextElementSibling.textContent = '', objValueForm.lastName = elem.value)
+                : (elem.nextElementSibling.textContent = 'Min length 3 simbol')
                 break;
             case 'exampleInputEmail1':
                 _regExpEmail.test(elem.value) ? (elem.nextElementSibling.textContent = '', objValueForm.email = elem.value) 
@@ -49,52 +46,49 @@ window.addEventListener('DOMContentLoaded', () => {
                 break;
             default:
                 break;
-        }
+        };
         console.log(objValueForm)
-        
-    }
 
-        for (let elements of form.elements) {
-            if (!elements.classList.contains('btn')) {
-                elements.addEventListener('input', () => {
-                    isValid(elements)
-                })
-        }
-        
     };
+           
+
+
+    for (let elements of form.elements) {
+        if (!elements.classList.contains('btn')) {
+            elements.addEventListener('input', () => {
+                isValid(elements)
+        })
+    }
+    
+};
+
 
 form.addEventListener('submit', submitHandler);
 
 function submitHandler(event) {
-
     event.preventDefault()
 
-    //    const objValueForm = {
-    //     firstName: firstName.value.trim(),
-    //     lastName: lastName.value.trim(),
-    //     email: email.value.trim(),
-    //     password: pass.value.trim() 
-    // }
+    // let isValidate = false
 
-    // postDB(objValueForm)
-
-        // lastName.value = '',
-        // firstName.value = '',
-        // email.value = '',
-        // pass.value = ''
-        // btnSubmit.disabled = true
-
-        for (let elements of form.elements) {
-            if (!elements.classList.contains('btn')) {
-                if (elements.value === '') {
-                    elements.nextElementSibling.textContent = 'This field is empty'
-            } else {
-                elements.nextElementSibling.textContent = ''
-            }
-        }
+    //     for (let elements of form.elements) {
+    //         if (!elements.classList.contains('btn')) {
+    //             if (elements.value === '') {
+    //                 elements.nextElementSibling.textContent = 'This field is empty'
+    //                 isValidate = false
+    //         } else {
+    //             elements.nextElementSibling.textContent = ''
+    //             isValidate = true
+    //         }
+    //     }
         
-    };
+    // };
+
+    // if (isValidate) {
+    //     submitForm(this.objValueForm)
+    // } else {
+    //     alert('Форма не отправлена')
+    // }
+    
 
   };
-
 });
